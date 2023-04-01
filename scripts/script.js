65f5e1cf4ab3b3062c37d183d09c503070dcee36
salimp2009@gmail.com <salimp2009@gmail.com>
2023-04-01 12:38:57 +0300

    Comment Web app with JS,CSS, html

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// COUNTER COMPONENT
+const textareaEl = document.querySelector(".form__textarea");
+const counterEl = document.querySelector(".counter");
+//
+const inputHandler = () => {
+  const maxNrChars = 150;
+  const nrCharsTyped = textareaEl.value.length;
+  const remainingChars = maxNrChars - nrCharsTyped;
+  counterEl.textContent = remainingChars;
+  console.log(remainingChars);
+};
+
+textareaEl.addEventListener("input", inputHandler);
